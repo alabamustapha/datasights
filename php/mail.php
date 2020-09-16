98947369e84b68b6f8ff9a4d1f92c79f172a939b
Mustapha Alaba <alabamustapha@gmail.com>
2020-09-16 14:48:01 +0100

    fix mailing

--- php/mail.php
+++ php/mail.php
@@ -1,17 +1,19 @@
-<!-- PHPMailer -->
-
 <?php
 
-    // Import PHPMailer classes into the global namespace
+     // Import PHPMailer classes into the global namespace
     // These must be at the top of your script, not inside a function
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
 
     // Load Composer's autoloader
-    require 'vendor/autoload.php';
+    require '../vendor/autoload.php';
     
-    if(isset($_POST['submit'])){
+    $name = isset($_POST['name']) ? $_POST['name'] : "";
+    $email = isset($_POST['email']) ? $_POST['email'] : "";
+    $message = isset($_POST['message']) ? $_POST['message'] : "";
+    
+    if(validate($name, $email, $message)){
 
         // Instantiation and passing `true` enables exceptions
         $mail = new PHPMailer(true);
@@ -20,36 +22,36 @@
             //Server settings
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
             $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'http://mail.redehub.com.ng/';                    // Set the SMTP server to send through
+            $mail->Host       = 'mail.redehub.com.ng';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'alabamustaph@redehub.com.ng';                     // SMTP username
-            $mail->Password   = 'Islam4you01/29';                               // SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Username   = 'alabamustapha@redehub.com.ng';                     // SMTP username
+            $mail->Password   = 'Islam4you';                               // SMTP password
+            // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
-            $mail->setFrom('alabamustaph@gmail.com', 'Mailer');
-            $mail->addAddress('adeniyiemmanuel617@gmail.com', 'Adeniyi Emmanuel');     // Add a recipient
-            $mail->addAddress('adeniyiemmanuel617@gmail.com');               // Name is optional
-            $mail->addReplyTo('alabamustaph@gmail.com', 'Information');
-            // $mail->addCC('cc@example.com');
-            // $mail->addBCC('bcc@example.com');
-
-            // // Attachments
-            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
+            $mail->setFrom('contact@datasights.ng', 'DataSights.ng - Contact Form');
+            $mail->addAddress($email, $name);     // Add a recipient
+            $mail->addReplyTo('contact@datasights.ng', 'DataSights');
+            
             // Content
             $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = "Mail from DataSights.ng - " . $name;
+            $mail->Body    = $message;
+            $mail->AltBody = $message;
 
             $mail->send();
-            echo 'Message has been sent';
+                echo 'Message has been sent';
             } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
 
+        }else{
+            echo 0;
+        }
+    
+
+    function validate($name, $email, $message){
+        return true;
     }
-?>
+    
