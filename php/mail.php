8a63405bba6a26d112b8c847e395c8beeef06c24
Alaba Mustapha <alabamustapha@gmail.com>
2020-09-17 15:17:22 +0100

    Updated email sending to use datasights details
    Updated email sending to use datasights details and updated form fields

--- php/mail.php
+++ php/mail.php
@@ -9,11 +9,11 @@
     // Load Composer's autoloader
     require '../vendor/autoload.php';
     
-    $name = isset($_POST['name']) ? $_POST['name'] : "";
+    $subject = isset($_POST['subject']) ? $_POST['subject'] : "";
     $email = isset($_POST['email']) ? $_POST['email'] : "";
     $message = isset($_POST['message']) ? $_POST['message'] : "";
     
-    if(validate($name, $email, $message)){
+    if(validate($subject, $email, $message)){
 
         // Instantiation and passing `true` enables exceptions
         $mail = new PHPMailer(true);
@@ -22,23 +22,23 @@
             //Server settings
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
             $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'mail.redehub.com.ng';                    // Set the SMTP server to send through
+            $mail->Host       = 'mail.datasights.ng';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'alabamustapha@redehub.com.ng';                     // SMTP username
-            $mail->Password   = 'Islam4you';                               // SMTP password
+            $mail->Username   = 'contact@datasights.ng';                     // SMTP username
+            $mail->Password   = 'YOURPASSWORD HERE';                               // SMTP password
             // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
             $mail->setFrom('contact@datasights.ng', 'DataSights.ng - Contact Form');
-            $mail->addAddress($email, $name);     // Add a recipient
-            $mail->addReplyTo('contact@datasights.ng', 'DataSights');
+            $mail->addAddress("contact@datasights.ng", "Contact Form");     // Add a recipient
+            $mail->addReplyTo($email);
             
             // Content
             $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = "Mail from DataSights.ng - " . $name;
-            $mail->Body    = $message;
-            $mail->AltBody = $message;
+            $mail->Subject = "Mail from DataSights.ng - " . $subject;
+            $mail->Body    = "Email: " . $email . "\n\n" . $message;
+            $mail->AltBody = "Email: " . $email . "\n\n" . $message;
 
             $mail->send();
                 echo 'Message has been sent';
@@ -53,9 +53,11 @@
 
     function validate($name, $email, $message){
 
-        if($name != " " && $email != " " && $message != " "){
-            return true;
+        if($name != " " && $email != " " && $message != " " && filter_var($email, FILTER_VALIDATE_EMAIL)){
+               return true;
         }
+         
+         return false;
     }
     
 
